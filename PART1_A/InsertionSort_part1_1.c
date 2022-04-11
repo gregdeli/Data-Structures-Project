74c7ff9f46576d58ca67036bad7ce91a91744095
diasakosd <103368009+diasakosd@users.noreply.github.com>
2022-04-12 01:10:01 +0300

    a

--- PART1_A/InsertionSort_part1_1.c
+++ PART1_A/InsertionSort_part1_1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Measurements{
+typedef struct String_Measurements{ //struct with array of strings
     char date[50],temp[50], phosphate[50];
     char silicate[50],  nitrite[50], nitrate[50];
     char salinity[50],  oxygen[50];
 
-}measurements;
+}string_measurements;
 
-typedef struct Measurements2{
+typedef struct Float_Measurements{ //struct with floats and string only for the date
     char date2[50];
     float temp2, phosphate2;
     float silicate2,  nitrite2, nitrate2;
     float salinity2,  oxygen2;
 
-}measurements2;
+}float_measurements;
 
 int main()
 {
@@ -30,8 +30,8 @@ int main()
     int row_count = 0;
     int field_count =0;
 
-    measurements values[1405];
-    measurements2 values2[1405];
+    string_measurements string_values[1405];
+    float_measurements float_values[1405];
     int i=0;
 
     while(fgets(buff1,sizeof(buff1),file))
@@ -46,21 +46,21 @@ int main()
             while(field)
             {
                 if(field_count==0)
-                    strcpy(values[i].date,field);
+                    strcpy(string_values[i].date,field);
                 if(field_count==1)
-                    strcpy(values[i].temp,field);
+                    strcpy(string_values[i].temp,field);      //while for copying from csv to struct
                 if(field_count==2)
-                    strcpy(values[i].phosphate,field);
+                    strcpy(string_values[i].phosphate,field);
                 if(field_count==3)
-                    strcpy(values[i].silicate,field);
+                    strcpy(string_values[i].silicate,field);
                 if(field_count==4)
-                    strcpy(values[i].nitrite,field);
+                    strcpy(string_values[i].nitrite,field);
                 if(field_count==5)
-                    strcpy(values[i].nitrate,field);
+                    strcpy(string_values[i].nitrate,field);
                 if(field_count==6)
-                    strcpy(values[i].salinity,field);
+                    strcpy(string_values[i].salinity,field);
                 if(field_count==7)
-                    strcpy(values[i].oxygen,field);
+                    strcpy(string_values[i].oxygen,field);
 
                 field = strtok(NULL, ",");
                 field_count++;
@@ -72,40 +72,54 @@ int main()
 
     for(int i=0; i<1405; i++)
     {
-       strcpy(values2[i].date2,values[i].date);
-       values2[i].temp2=atof(values[i].temp);
-       values2[i].phosphate2=atof(values[i].phosphate);
-       values2[i].silicate2=atof(values[i].silicate);
-       values2[i].nitrite2=atof(values[i].nitrite);
-       values2[i].nitrate2=atof(values[i].nitrate);
-       values2[i].salinity2=atof(values[i].salinity);
-       values2[i].oxygen2=atof(values[i].oxygen);
+       strcpy(float_values[i].date2,string_values[i].date);
+       float_values[i].temp2=atof(string_values[i].temp);
+       float_values[i].phosphate2=atof(string_values[i].phosphate);
+       float_values[i].silicate2=atof(string_values[i].silicate);       //for loop for converting the strings to floats in order to compare them
+       float_values[i].nitrite2=atof(string_values[i].nitrite);
+       float_values[i].nitrate2=atof(string_values[i].nitrate);
+       float_values[i].salinity2=atof(string_values[i].salinity);
+       float_values[i].oxygen2=atof(string_values[i].oxygen);
 
     }
 
-   // printMeasurments(values);
-    printMeasurments2(values2);
+   // printMeasurments(string_values);
+  //printMeasurments2(float_values);       //functions in order to print the string data struct or the float_string data struct.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     return 0;
 }
 
-void printMeasurments(measurements values[])
+void printMeasurments(string_measurements string_values[])
 {
     for(int i=0; i<1405; i++)
     {
-        printf("%s %s %s %s ",values[i].date,values[i].temp,values[i].phosphate,values[i].silicate);
-        printf("%s %s %s %s \n",values[i].nitrite,values[i].nitrate,values[i].salinity,values[i].oxygen);
+        printf("%s %s %s %s ",string_values[i].date,string_values[i].temp,string_values[i].phosphate,string_values[i].silicate);
+        printf("%s %s %s %s \n",string_values[i].nitrite,string_values[i].nitrate,string_values[i].salinity,string_values[i].oxygen);
 
     }
 }
 
-void printMeasurments2(measurements2 values2[])
+void printMeasurments2(float_measurements float_values[])
 {
     for(int i=0; i<1405; i++)
     {
-       printf("%s %.2f %.2f %.2f ",values2[i].date2,values2[i].temp2,values2[i].phosphate2,values2[i].silicate2);
-       printf("%.2f %.2f %.2f %.2f \n",values2[i].nitrite2,values2[i].nitrate2,values2[i].salinity2,values2[i].oxygen2);
+       printf("%s %.2f %.2f %.2f ",float_values[i].date2,float_values[i].temp2,float_values[i].phosphate2,float_values[i].silicate2);
+       printf("%.2f %.2f %.2f %.2f \n",float_values[i].nitrite2,float_values[i].nitrate2,float_values[i].salinity2,float_values[i].oxygen2);
 
     }
 }
